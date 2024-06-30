cfcb7e1812fa69f30ba555532b273eeda7a3b36a
bhakti <bhaktishinde2388@gmail.com>
2024-07-01 00:46:10 +0530

    structure of todo container is created

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,10 +1,14 @@
+import "./Home.css"
+import CheckIcon from "./icons/checklist.png"
 function Home()
 {
     return(
         <>
-        <h1>
-            hello react
-        </h1>
+        <h1 className="heading"> todo list</h1>
+        <div className="todo-container">
+          
+          <img className="check-icon" src={CheckIcon}/>
+        </div>
         </>
     )
 }
