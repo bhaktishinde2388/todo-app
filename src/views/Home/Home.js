198872657ffaad7f868014f4391c3e2fd75f745a
bhakti <bhaktishinde2388@gmail.com>
2024-07-01 15:35:20 +0530

    added one add button and input box at the bottom of task container

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,21 +1,28 @@
 import "./Home.css"
 import CheckIcon from "./icons/checklist.png"
 import ToDoCards from "../../components/ToDoCards/ToDoCards"
+import { useState } from "react"
 
 function Home()
 {
-    const myList = [
-        "Goto to Market",
-       "fill the bootle",
-       "complete the assignment",
-       "Goto to Market",
-       "fill the bootle",
-       "complete the assignment",
-       "Goto to Market",
-       "fill the bootle",
-       "complete the assignment",
+    // const todoList = [
+    //     "Goto to Market",
+    //    "fill the bootle",
+    //    "complete the assignment",
+    //    "Goto to Market",
+    //    "fill the bootle",
+    //    "complete the assignment",
+    //    "Goto to Market",
+    //    "fill the bootle",
+    //    "complete the assignment",
+    //    "complete the assignment",
+    //    "Goto to Market",
+    //    "fill the bootle",
+    //    "complete the assignment",
         
-    ]
+    // ]
+
+    const [todoList,setTodoList] =useState([])
     return(
         
         <div>
@@ -27,14 +34,14 @@ function Home()
        <div className="to-do-main-container">
           <div className="todo-list-container">
             {
-                myList.map((todoItem, i)=><ToDoCards key={i} todoItem={todoItem}/>)
+                todoList.map((todoItem, i)=><ToDoCards key={i} todoItem={todoItem}/>)
                 // myList.map((todoItem , i)=>{
                 //    return(
                 //         <div key={i}>{todoItem}</div>
                 //     )
                 // }) 
             }
-             <div>   <img className="check-icon" src={CheckIcon} />   </div>
+             <div>   <img className="check-icon" src={CheckIcon}/>   </div>
            </div>
 
            
@@ -42,8 +49,12 @@ function Home()
           </div>
        
 
-        <div>
-            <input type="text" value={""}/>
+        <div className="add-task-container">
+            <input type="text"
+             className="add-input-box"
+             placeholder="add"
+             />
+            <button type="button" className="add-btn">Add</button>
         </div>
 
 
