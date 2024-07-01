0ba5b612f83fe2a6f4ebe22c56fddc432d8cc336
bhakti <bhaktishinde2388@gmail.com>
2024-07-01 23:05:48 +0530

    toaster added

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -2,6 +2,7 @@ import "./Home.css"
 import CheckIcon from "./icons/checklist.png"
 import ToDoCards from "../../components/ToDoCards/ToDoCards"
 import { useState } from "react"
+import toast, { Toaster} from 'react-hot-toast';
 
 function Home()
 {
@@ -30,22 +31,33 @@ function Home()
 
 
 
-        <h1 className="heading"> todo list</h1>
+        <h1 className="heading"> Todo list</h1>
 
        <div className="to-do-main-container">
           <div className="todo-list-container">
             {
-                todoList.map((todoItem, i)=><ToDoCards key={i} todoItem={todoItem}/>)
-                // myList.map((todoItem , i)=>{
+                 // myList.map((todoItem , i)=>{
                 //    return(
                 //         <div key={i}>{todoItem}</div>
                 //     )
                 // }) 
-            }
+                todoList.map((todoItem, i)=><ToDoCards key={i} todoItem={todoItem}/>)}
+               
+
+                {
+                    todoList.length === 0
+                    ?
+                    <span className="empty-task-container-text">
+                    There is no task added.........<br></br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                    .......🙄😪........<br></br>
+                    please add some task🤠🤠
+                    </span>
+                    :null
+                }
+            
              <div>   <img className="check-icon" src={CheckIcon}/>   </div>
            </div>
-
-           
+         
 
           </div>
        
@@ -53,24 +65,28 @@ function Home()
         <div className="add-task-container">
             <input type="text"
              className="add-input-box"
-             placeholder="add task"
-             value={newTask}
+             placeholder="add new task"
              onChange={(b)=>setNewTask(b.target.value)}
+             value={newTask}
              />
             <button type="button"
              className="add-btn"
              onClick={()=>{
-                setTodoList([
-                        ...todoList,newTask
-             ])
-                console.log(newTask)
+                 if(newTask == ""){
+                    toast.error("task can't add");
+                 }
+
+
+                setTodoList([ ...todoList,newTask  ])
+                setNewTask("")
+                toast.success("work submitted")
              }}
              
              >Add</button>
         </div>
 
 
-
+<Toaster/>
         </div>
         
     )
